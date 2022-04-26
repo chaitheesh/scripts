45c7ceebcb78cf1d325b42a0bff68f69fefc7c88
chaitheesh <chaitheesh@gmail.com>
2022-04-26 19:10:54 +0530

    Update range-info-puller-v1.sh

--- range-info-puller-v1.sh
+++ range-info-puller-v1.sh
@@ -1,11 +1,14 @@
-#!/bin/bash
+#!/bin/sh
+#version=1
+#Cheking whether the host is part of any cf3 ranges or not.
+#Removing host mentioned as it is in cf3 ranges.
 RED='\033[0;31m'
 NC='\033[0m'
-#cheking whether the host is part of any CF3 ranges
 #installing gnu-sed for MAC usage
 #brew install gnu-sed;
 #exporting gnu_sed path
 #export PATH="/usr/local/opt/gnu-sed/libexec/gnubin:$PATH";
+#Arranging the entered hosts into one by one
 tr "," "\n" < $1 | awk -F "'" '{print $2}' > tmp-file
 mv tmp-file $1 ;
 #cat $1;
@@ -20,16 +23,12 @@ for i in `cat $1`;do
         eh -e %index:$i > $hmdir/tmp-out-cf3-$1;
         cat $hmdir/tmp-out-cf3-$1 | grep cf3 > /dev/null ; # >> $hmdir/tmp-all-cf3-foundin-file-$1 ;
         if [ `echo $?` == 0 ]; then
-		#cd /export/home/eng/cthamaba/mysvn/trunk;
 		cd $repodir;
-		#svn info;
-		#svn up;
 		#echo -e "\n Host $i is part of cf3 ranges";
 		for j in `cat $hmdir/tmp-out-cf3-$1 | grep cf3`;do
-			echo $j;
+			#echo $j;
 			grep -in -w $i $j.yaml > /dev/null;
 				if [ `echo $?` == 0 ];then
-					echo "*";
 					#grep -in -w $i /export/home/eng/cthamaba/mysvn/trunk/$j.yaml  >> tmp-found-host-asitisin-cf3-rangefile-$1;
 					#grep -in -w $i $j.yaml  >> $hmdir/tmp-found-host-asitisin-cf3-rangefile-$1;
 					echo  "$i hostname found as it is in range $j" >> $hmdir/HOST-found-asitisin-cf3range-file-$1;
@@ -43,22 +42,23 @@ for i in `cat $1`;do
 			#	fi
 			#done
 			for l in `grep "%" $j.yaml | grep -v cf3`; do
-				eh -e $l | grep $i ;
+				eh -e $l | grep $i > /dev/null ;
 				if [ `echo $?` == 0 ]; then
-					echo "*";
 					#echo "tmp-found-subrange-of-sub-rangefile";
 					echo  "$i hostname found in subrange $l and part of $j" >> $hmdir/HOST-foundin-subrange-file-$1;
 				fi
 			done
 			#for m in `cat $hmdir/series-ranges `;do
 				#if [ $j == $m ] ; then
-			grep  "\.\." $j.yaml ; # > /dev/null;
+			grep  "\.\." $j.yaml > /dev/null ; # > /dev/null;
 				if [ `echo $?` == 0 ]; then
-					eh -e %"$j" | grep $i ;
+					eh -e %"$j" | grep $i > /dev/null;
 					if [ `echo $?` == 0 ]; then
-						echo "*";
 						#echo "Range $j is part of series ";
-						echo  "$i hostname found in $j range and in series entries" >>  $hmdir/HOST-foundin-series-file-$1;
+						grep -w $i $hmdir/HOST-found-asitisin-cf3range-file-$1 $hmdir/HOST-foundin-subrange-file-$1 | grep -w $j > /dev/null;
+						if [ `echo $?` != 0 ]; then
+							echo  "$i hostname found in $j range and in series entries" >>  $hmdir/HOST-foundin-series-file-$1;
+						fi
 					fi
 				fi
 			#done
@@ -99,7 +99,7 @@ for n in `ls $hmdir | grep $1| grep -v ^$1`;do
 		if [ $count -ge 10 ];then
 			echo "\n ${RED}  Host as it is entries are more than 10, so please do it manually. Host details are in file $hmdir/$n ${NC}";
 		else
-			#echo "\n less than 10"
+			echo "\n less than 10"
 			remove_hosts $n;
 		fi
 	elif [ $n == "HOST-foundin-subrange-file-$1" ] ; then
